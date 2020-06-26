045e8d1ec9755f3cb28a97d0be671a9a570f0731
Pankaj Chouhan <pchouhan3@gatech.edu>
2020-06-26 19:44:57 -0400

    Added to get the form data and access it in activity classes

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CurrentJobDetails.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CurrentJobDetails.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CurrentJobDetails extends AppCompatActivity implements View.OnClickListener {
+    EditText title, company, city, state, costOfLiving, yearlySalary, signingBonus, yearlyBonus, retirementBonus, leaveTime;
+    Button save, cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,25 @@ public class CurrentJobDetails extends AppCompatActivity implements View.OnClick
                 cancelCurrentJobDetails();
                 break;    
         }
+    }
+    
+    private void init(){
+        title = (EditText)findViewById(R.id.txt_current_job_details_title);
+        company = (EditText)findViewById(R.id.txt_current_job_details_company);
+        city = (EditText)findViewById(R.id.txt_current_job_details_city);
+        state = (EditText)findViewById(R.id.txt_current_job_details_state);
+        costOfLiving = (EditText)findViewById(R.id.txt_current_job_details_costofliving);
+        yearlySalary = (EditText)findViewById(R.id.txt_current_job_details_yearly_salary);
+        signingBonus = (EditText)findViewById(R.id.txt_current_job_details_signing_bonus);
+        yearlyBonus = (EditText)findViewById(R.id.txt_current_job_details_yearly_bonus);
+        retirementBonus = (EditText)findViewById(R.id.txt_current_job_details_retirement_bonus);
+        leaveTime = (EditText)findViewById(R.id.txt_current_job_details_leave_time);
+
+        save = (Button)findViewById(R.id.btn_current_job_details_save);
+        cancel = (Button)findViewById(R.id.btn_current_job_details_cancel);
+
+        save.setOnClickListener(this);
+        cancel.setOnClickListener(this);
     }
 
     private void cancelCurrentJobDetails() {
--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/JobOffers.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/JobOffers.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class JobOffers extends AppCompatActivity implements View.OnClickListener {
+    EditText title, company, city, state, costOfLiving, yearlySalary, signingBonus, yearlyBonus, retirementBonus, leaveTime;
+    Button add, save, compare, cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_offers);
+        init();
     }
 
     @Override
@@ -31,15 +37,42 @@ public class JobOffers extends AppCompatActivity implements View.OnClickListener
         }
     }
 
+    private void init(){
+        title = (EditText)findViewById(R.id.txt_job_offers_title);
+        company = (EditText)findViewById(R.id.txt_job_offers_company);
+        city = (EditText)findViewById(R.id.txt_job_offers_city);
+        state = (EditText)findViewById(R.id.txt_job_offers_state);
+        costOfLiving = (EditText)findViewById(R.id.txt_job_offers_costofliving);
+        yearlySalary = (EditText)findViewById(R.id.txt_job_offers_yearly_salary);
+        signingBonus = (EditText)findViewById(R.id.txt_job_offers_signing_bonus);
+        yearlyBonus = (EditText)findViewById(R.id.txt_job_offers_yearly_bonus);
+        retirementBonus = (EditText)findViewById(R.id.txt_job_offers_retirement_bonus);
+        leaveTime = (EditText)findViewById(R.id.txt_job_offers_leave_time);
+
+        add = (Button)findViewById(R.id.btn_job_offers_add);
+        save = (Button)findViewById(R.id.btn_job_offers_save);
+        compare = (Button)findViewById(R.id.btn_job_offers_compare);
+        cancel = (Button)findViewById(R.id.btn_job_offers_cancel);
+
+        add.setOnClickListener(this);
+        save.setOnClickListener(this);
+        compare.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+    }
+
     private void compareJobOffers() {
+//        System.out.println(title.getText().toString() + company.getText().toString() + city.getText().toString() + state.getText().toString() + costOfLiving.getText().toString() + yearlySalary.getText().toString() + signingBonus.getText().toString() + yearlyBonus.getText().toString() + retirementBonus.getText().toString() + leaveTime.getText().toString());
     }
 
     private void addJobOffers() {
+//        System.out.println(title.getText().toString() + company.getText().toString() + city.getText().toString() + state.getText().toString() + costOfLiving.getText().toString() + yearlySalary.getText().toString() + signingBonus.getText().toString() + yearlyBonus.getText().toString() + retirementBonus.getText().toString() + leaveTime.getText().toString());
     }
 
     private void cancelJobOffers() {
+//        System.out.println(title.getText().toString() + company.getText().toString() + city.getText().toString() + state.getText().toString() + costOfLiving.getText().toString() + yearlySalary.getText().toString() + signingBonus.getText().toString() + yearlyBonus.getText().toString() + retirementBonus.getText().toString() + leaveTime.getText().toString());
     }
 
     private void saveJobOffers() {
+//        System.out.println(title.getText().toString() + company.getText().toString() + city.getText().toString() + state.getText().toString() + costOfLiving.getText().toString() + yearlySalary.getText().toString() + signingBonus.getText().toString() + yearlyBonus.getText().toString() + retirementBonus.getText().toString() + leaveTime.getText().toString());
     }
 }
