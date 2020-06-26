528a5fabccde27f6f066afbfeff4afe6ba6be75b
Pankaj Chouhan <pchouhan3@gatech.edu>
2020-06-26 19:06:30 -0400

    Modified corresponding java classes for activities

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CurrentJobDetails.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CurrentJobDetails.java
@@ -3,12 +3,31 @@ package edu.gatech.seclass.jobcompare6300;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class CurrentJobDetails extends AppCompatActivity {
+public class CurrentJobDetails extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_job_details);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_current_job_details_save:
+                saveCurrentJobDetails();
+                break;
+            case R.id.btn_current_job_details_cancel:
+                cancelCurrentJobDetails();
+                break;    
+        }
+    }
+
+    private void cancelCurrentJobDetails() {
+    }
+
+    private void saveCurrentJobDetails() {
     }
 }
--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/JobOffers.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/JobOffers.java
@@ -3,12 +3,43 @@ package edu.gatech.seclass.jobcompare6300;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class JobOffers extends AppCompatActivity {
+public class JobOffers extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_offers);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_job_offers_save:
+                saveJobOffers();
+                break;
+            case R.id.btn_job_offers_cancel:
+                cancelJobOffers();
+                break;
+            case R.id.btn_job_offers_add:
+                addJobOffers();
+                break;
+            case R.id.btn_job_offers_compare:
+                compareJobOffers();
+                break;
+        }
+    }
+
+    private void compareJobOffers() {
+    }
+
+    private void addJobOffers() {
+    }
+
+    private void cancelJobOffers() {
+    }
+
+    private void saveJobOffers() {
     }
 }
