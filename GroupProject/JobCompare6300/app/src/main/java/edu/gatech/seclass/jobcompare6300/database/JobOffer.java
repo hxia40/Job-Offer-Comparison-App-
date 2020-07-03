550c47d75c252c937506068a0117fc40d1eefd4d
Wood, Richard Ezekiel <zeke@gatech.edu>
2020-07-02 17:56:18 -0700

    remaining attributes

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/database/JobOffer.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/database/JobOffer.java
@@ -38,5 +38,100 @@ public class JobOffer {
 
     @ColumnInfo(name = "city")
     private String city;
+    
+    // state
+	//@ColumnInfo(name = "state")
+    //private String state;
+
+	//public String getstate() {
+    //    return state;
+    //}
+
+    //public void setstate(String state) {
+    //    this.state = state;
+    //}
+	
+	
+	
+	// costOfLiving
+	//@ColumnInfo(name = "costOfLiving")
+    //private double costOfLiving;
+
+	//public double getcostOfLiving() {
+    //    return costOfLiving;
+    //}
+
+    //public void setcostOfLiving(double costOfLiving) {
+    //    this.costOfLiving = costOfLiving;
+    //}
+	
+	
+	// yearlySalary
+	//@ColumnInfo(name = "yearlySalary")
+    //private double yearlySalary;
+
+	//public double getyearlySalary() {
+    //    return yearlySalary;
+    //}
+
+    //public void setyearlySalary(double yearlySalary) {
+    //    this.yearlySalary = yearlySalary;
+    //}
+	
+	
+	// signingBonus
+	//@ColumnInfo(name = "signingBonus")
+    //private double signingBonus;
+
+	//public double getsigningBonus() {
+    //    return signingBonus;
+    //}
+
+    //public void setyearlySalary(double signingBonus) {
+    //    this.signingBonus = signingBonus;
+    //}
+	
+	
+	
+	// yearlyBonus
+	//@ColumnInfo(name = "yearlyBonus")
+    //private double yearlyBonus;
+
+	//public double getyearlyBonus() {
+    //    return yearlyBonus;
+    //}
+
+    //public void setyearlyBonus(double yearlyBonus) {
+    //    this.yearlyBonus = yearlyBonus;
+    //}
+	
+	
+	
+	
+	
+	// retiremnetBenifits
+	//@ColumnInfo(name = "retiremnetBenifits")
+    //private float retiremnetBenifits;
+
+	//public float getretiremnetBenifits() {
+    //    return retiremnetBenifits;
+    //}
+
+    //public void setretiremnetBenifits(float retiremnetBenifits) {
+    //    this.retiremnetBenifits = retiremnetBenifits;
+    //}
+	
+	
+	// leaveTime
+	//@ColumnInfo(name = "leaveTime")
+    //private int leaveTime;
+
+	//public int getleaveTime() {
+    //    return leaveTime;
+    //}
+
+    //public void setleaveTime(int leaveTime) {
+    //    this.leaveTime = leaveTime;
+    //}
 
 }
