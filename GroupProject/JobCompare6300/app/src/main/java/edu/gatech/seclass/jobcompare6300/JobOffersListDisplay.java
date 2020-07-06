bcee30703f14276f9304b3afb7f8d723c94ec414
shariharan33 <shariharan33@gatech.edu>
2020-07-05 23:25:42 -0700

    resolving merge issues

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/JobOffersListDisplay.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/JobOffersListDisplay.java
@@ -112,14 +112,14 @@ public class JobOffersListDisplay extends AppCompatActivity {
 //        String[] company = DAI.readCompany(weights.yearlySalaryWeight,weights.signingBonusWeight,weights.yearlyBonusWeight,weights.retirementBenefitsWeight,weights.leaveTimeWeight,sum);
 //        String[] title = DAI.readTitle(weights.yearlySalaryWeight,weights.signingBonusWeight,weights.yearlyBonusWeight,weights.retirementBenefitsWeight,weights.leaveTimeWeight,sum);
 //        String[] city = DAI.readCity(weights.yearlySalaryWeight,weights.signingBonusWeight,weights.yearlyBonusWeight,weights.retirementBenefitsWeight,weights.leaveTimeWeight,sum);
-        int size = DAI.readsize();
+        int size = DAI.readsizeNonCurrent();
         String[] items = new String[size];
 //        int currentId = DAI.getCurrentJobId();
 
         int[] jobOffers = DAI.readOfferNonCurrent(weights.yearlySalaryWeight,weights.signingBonusWeight,weights.yearlyBonusWeight,weights.retirementBenefitsWeight,weights.leaveTimeWeight,sum);
 
         for (int i=0; i< size; i++) {
-                items[i] = jobOffers[i] + " " + DAI.getTitleById(jobOffers[i]) + " " + DAI.getCompanyById(jobOffers[i]) + " " + DAI.getCityById(jobOffers[i]);
+            items[i] = jobOffers[i] + " " + DAI.getTitleById(jobOffers[i]) + " " + DAI.getCompanyById(jobOffers[i]) + " " + DAI.getCityById(jobOffers[i]);
         }
 
 //        for(int i=0; i < size; i++){
